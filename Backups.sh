44764bfe303adf59eeaab571dbb429e14b8e99ac
N4ND-0 <84549508+N4ND-0@users.noreply.github.com>
2021-06-05 23:33:04 -0300

    Update Backups.sh

--- Backups.sh
+++ Backups.sh
@@ -2,15 +2,15 @@
 
 echo -e "This script performs a backup of the files in your current folder:"
 ls -1
-DATA="$(date +%d.%m.%y.%H.%M)"
-PASTA="/tmp/backup/$DATA"
-echo -e "Creating a temporary folder ${PASTA}..."
-mkdir -p ${PASTA} 2> /dev/null
+DATE="$(date +%d.%m.%y.%H.%M)"
+PAST="/tmp/backup/$DATE"
+echo -e "Creating a temporary folder ${PAST}..."
+mkdir -p ${PAST} 2> /dev/null
 echo -e "Copying the files (except the .zip) .. was"
-cp $(ls | grep -v *.zip) $PASTA
+cp $(ls | grep -v *.zip) $PAST
 echo -e "Now I'm compressing the files"
-zip -r ./${DATA}.zip ${PASTA}/*
-echo -e "deleting a temporary folder ${PASTA}..."
-rm -r ${PASTA} 2> /dev/null
-echo -e "Feito!"
+zip -r ./${DATE}.zip ${PAST}/*
+echo -e "deleting a temporary folder ${PAST}..."
+rm -r ${PAST} 2> /dev/null
+echo -e "NICE!"
 
